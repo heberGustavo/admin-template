f0be48083a727d0acb23f5c7d31eea1874c13c70
heberGustavo <heber.gbarbosa@hotmail.com>
2024-09-13 08:26:10 -0300

    Import Tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 export default function Home() {
   return (
-    <div>
+    <div className={
+      `
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-green-500 to-blue-500
+      `
+    }>
       <h1>
         Admin
       </h1>
