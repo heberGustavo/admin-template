64c2865f3192a9de6a0c36cfe95774b464703559
heberGustavo <heber.gbarbosa@hotmail.com>
2024-09-16 08:23:52 -0300

    Add style to page

--- src/components/template/Titulo.tsx
+++ src/components/template/Titulo.tsx
@@ -1,19 +1,17 @@
-interface TituloProps{
+interface TituloProps {
     titulo: string;
     subtitulo: string;
 }
 
-export default function Layout(props: TituloProps){
-    return(
+export default function Layout(props: TituloProps) {
+    return (
         <div>
-            <h1 className={`
-            `}>
+            <h1 className={`font-black text-3xl text-gray-900`}>
                 {props.titulo}
-                </h1>
-            <h2 className={`
-            `}>
+            </h1>
+            <h2 className={`font-light text-sm text-gray-600`}>
                 {props.subtitulo}
-                </h2>
+            </h2>
         </div>
     );
 }
