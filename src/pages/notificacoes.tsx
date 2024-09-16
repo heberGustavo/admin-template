617bd0608f943206e3626d344183c9380414a132
heberGustavo <heber.gbarbosa@hotmail.com>
2024-09-16 08:18:14 -0300

    Create page base

---
+++ src/pages/notificacoes.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/template/Layout";
+
+export default function Notificacoes() {
+  return (
+    <Layout titulo="Notificacoes" subtitulo="Aqui você irá gerenciar as suas notificações">
+      <h3>Conteudo</h3>
+    </Layout>
+  );
+}
