689bbb930aee749e6132e5a40a794d30d8c943b6
heberGustavo <heber.gbarbosa@hotmail.com>
2024-09-17 08:11:17 -0300

    Add custom to dark mode

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -16,11 +16,11 @@ export default function MenuItem(props: MenuItemProps) {
                 <a className={`
                     flex flex-col justify-center items-center
                     h-20 w-20
-                    text-gray-600
+                    dark:text-gray-200
                     ${props.className}
                 `}>
                     {props.icone}
-                    <span className={`text-xs font-ligh`}>
+                    <span className={`text-xs font-light`}>
                         {props.texto}
                     </span>
                 </a>
@@ -29,7 +29,10 @@ export default function MenuItem(props: MenuItemProps) {
     }
 
     return (
-        <li onClick={props.onClick} className={`hover:bg-gray-100 cursor-pointer`}>
+        <li onClick={props.onClick} className={`
+            cursor-pointer
+            hover:bg-gray-100 dark:hover:bg-gray-800
+        `}>
             {
                 props.url ? (
                     <Link legacyBehavior
--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -2,9 +2,13 @@ import { IconeAjustes, IconeCasa, IconeSair, IconeSino } from "../icons";
 import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
-export default function MenuLateral(){
-    return(
-        <aside className="flex flex-col">
+export default function MenuLateral() {
+    return (
+        <aside className={`
+            flex flex-col
+            bg-gray-200 text-gray-700
+            dark:bg-gray-900
+        `}>
             <div className={`
                 flex flex-col items-center justify-center
                 bg-gradient-to-r from-indigo-500 to-purple-800
@@ -18,12 +22,13 @@ export default function MenuLateral(){
                 <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
             </ul>
             <ul>
-                <MenuItem 
-                    texto="Sair" 
-                    icone={IconeSair} 
+                <MenuItem
+                    texto="Sair"
+                    icone={IconeSair}
                     className={`
                         text-red-600
                         hover:bg-red-400 hover:text-white
+                        dark:text-red-400 dark:hover:text-white
                     `}
                     onClick={() => alert("logout")} />
             </ul>
