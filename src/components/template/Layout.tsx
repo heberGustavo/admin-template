fbbe39bf4e245183234ec73a83f9e0a1ce1d94fd
heberGustavo <heber.gbarbosa@hotmail.com>
2024-09-26 08:13:31 -0300

    Protecting routes with function

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -3,7 +3,8 @@ import Cabecalho from "./Cabecalho";
 import Conteudo from "./Conteudo";
 import { ReactNode } from "react";
 import useAppData from "@/data/hook/useAppData";
-import ForcarAutenticacao from "../auth/ForcarAutenticacao";
+import forcarAutenticacao from "@/functions/ForcarAutenticacao";
+//import ForcarAutenticacao from "../auth/ForcarAutenticacao";
 
 interface LayoutProps {
     titulo: string;
@@ -14,20 +15,18 @@ interface LayoutProps {
 export default function Layout(props: LayoutProps) {
     const { tema } = useAppData();
 
-    return (
-        <ForcarAutenticacao>
-            <div className={`${tema} flex h-screen w-screen`}>
-                <MenuLateral />
-                <div className={`
-                flex flex-col w-full p-7
-                bg-gray-300 dark:bg-gray-800
-            `}>
-                    <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
-                    <Conteudo>
-                        {props.children}
-                    </Conteudo>
-                </div>
+    return forcarAutenticacao(
+        <div className={`${tema} flex h-screen w-screen`}>
+            <MenuLateral />
+            <div className={`
+            flex flex-col w-full p-7
+            bg-gray-300 dark:bg-gray-800
+        `}>
+                <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
+                <Conteudo>
+                    {props.children}
+                </Conteudo>
             </div>
-        </ForcarAutenticacao>
-    );
+        </div>
+    )
 }
